20e877c88c00fd50f2f6afd2a7a49a65fc5542ff
uddesh-kr-bhagat <136488707+uddesh-kr-bhagat@users.noreply.github.com>
2024-07-10 13:31:52 +0530

    "finished animation"

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,38 @@
 import React from 'react'
 import about from '../assets/about.jpg'
 import { ABOUT_TEXT } from '../constants'
+import { motion } from 'framer-motion'
 const About = () => {
   return (
     <div className='border-b border-neutral-900 pb-4'>
-        <h1 className='my-20 text-center text-4xl'>About
+        <motion.h1
+        whileInView={{opacity:1,y:0}}
+        initial={{opacity:0,y:-100}}
+        transition={{duration:1.5}} 
+        className='my-20 text-center text-4xl'>About
             <span className='text-neutral-500'>Me</span>
-        </h1>
+        </motion.h1>
         <div className='flex flex-wrap'>
-            <div className='w-full lg:w-1/2 lg:p-8'>
+            <motion.div 
+            whileInView={{opacity:1, x:0}}
+            initial={{opacity:0,x:-100}}
+            transition={{duration:0.5}}
+            className='w-full lg:w-1/2 lg:p-8'>
                 <div className='flex items-center justify-center lg:justify-center'>
                 <img src={about} style={{ width: '60%' }} className='mix-blend-lighten rounded-full' alt='about' />
                 </div>
-            </div>
-            <div className='w-full lg:w-1/2'>
+            </motion.div>
+            <motion.div
+            whileInView={{opacity:1, x:0}}
+            initial={{opacity:0,x:100}}
+            transition={{duration:0.5}} 
+            className='w-full lg:w-1/2'>
                 <div className='flex justify-center lg:justify-start'>
                 <p className='my-2 max-w-xl py-6'>
                     {ABOUT_TEXT}
                 </p>
                 </div>
-            </div>
+            </motion.div>
         </div>
     </div>
   )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { PROJECTS } from '../constants'
-
+import { motion } from 'framer-motion'
 const Projects = () => {
   return (
     <div className='border-b border-neutral-800 pb-4'>
-        <h1 className='my-20 text-center text-4xl'>
+        <motion.h1 
+        whileInView={{opacity:1,y:0}}
+        initial={{opacity:0,y:-100}}
+        transition={{duration:1}} 
+        className='my-20 text-center text-4xl'>
             Projects
-        </h1>
+        </motion.h1>
         <div>
             {PROJECTS.map((project,index)=>(
                 <div key={index} className='mb-8 flex flex wrap lg:justify-center'>
-                    <div className='w-full lg:w-1/4'>
+                    <motion.div 
+                    whileInView={{opacity:1,x:0}}
+                    initial={{opacity:0,x:-100}}
+                    transition={{duration:1}} 
+                    className='w-full lg:w-1/4'>
                         <img 
                         src={project.image} 
                         alt={project.title}
@@ -18,14 +26,18 @@ const Projects = () => {
                         height={150}
                         className='mb-6 rounded'
                         />
-                    </div>
-                    <div className='w-full max-w-xl lg:w-3/4'>
+                    </motion.div>
+                    <motion.div 
+                    whileInView={{opacity:1,x:0}}
+                    initial={{opacity:0,x:100}}
+                    transition={{duration:1}} 
+                    className='w-full max-w-xl lg:w-3/4'>
                         <h6 className='mb-2 font-semibold'>{project.title}</h6>
                         <p className='mb-4 text-neutral-400'>{project.description}</p>
                         {project.technologies.map((tech,index)=>(
                             <span key={index} className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>{tech}</span>
                         ))}
-                    </div>
+                    </motion.div>
                 </div>    
             ))}
         </div>    
